d4263b3b263f33dc018bd230ada8ada5e2846615
yutingcxiang <yutingcxiang@gmail.com>
2018-11-24 01:53:19 -0500

    Add Article model and controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,19 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+    render json: @articles, status: 200
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    render json: @article, status: 200
+  end
+
+  
+
+  private
+
+  def article_params
+    params.require(:article).permit()
+  end
+end
