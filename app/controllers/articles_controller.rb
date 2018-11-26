91dd04c3c426d97896530c7640b9175f95c91e93
yutingcxiang <yutingcxiang@gmail.com>
2018-11-25 20:08:41 -0500

    Setup top_headlines api route for top 20 headlines in the US

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,19 @@ class ArticlesController < ApplicationController
     render json: @articles, status: 200
   end
 
-  def show
-    @article = Article.find(params[:id])
-    render json: @article, status: 200
+  def top_headlines
+    response = Faraday.get 'https://newsapi.org/v2/top-headlines?' do |req|
+      req.params['apiKey'] = ENV['API_KEY']
+      req.params['country'] = 'us'
+    end
+    @articles = JSON.parse(response.body)
+    render json: @articles, status: 200
+  end
+
+  def all_news
+  end
+
+  def news_sources
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope :api do
-    resources :articles 
+    resources :articles
+    get '/top_headlines', to: 'articles#top_headlines' 
   end
 end
