b9e609e9333b07e658a43f48865322488bbd0851
yutingcxiang <yutingcxiang@gmail.com>
2018-11-27 02:46:10 -0500

    Adjust layout of App.js

--- client/src/reducers/articles_reducer.js
+++ client/src/reducers/articles_reducer.js
@@ -1,7 +1,6 @@
 export default function articlesReducer(state = {loading: false, articles: []}, action) {
   switch (action.type) {
     case 'LOADING_ARTICLES':
-      console.log(action.payload);
       return {...state, loading: true}
 
     case 'FETCH_HEADLINE_ARTICLES':
