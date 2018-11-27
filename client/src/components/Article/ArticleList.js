2dd58c7e1434224024bbdc9daab707fd39fee3e4
yutingcxiang <yutingcxiang@gmail.com>
2018-11-27 01:57:31 -0500

    Update articles controller and routes

--- client/src/components/Article/ArticleList.js
+++ client/src/components/Article/ArticleList.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ArticleCard from './ArticleCard'
 
 const ArticleList = () => {
+  // const articles = this.props.articles.map(article => <ArticleCard />)
+
   return(
     <div>
     ArticleList
-    <ArticleCard />
     </div>
   )
 }
