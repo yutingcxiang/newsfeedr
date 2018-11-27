f9ddfc82a088af1a647231597bb833b6140657e1
yutingcxiang <yutingcxiang@gmail.com>
2018-11-27 01:08:12 -0500

    Add action for fetching filtered news articles

--- client/src/actions/articleActions.js
+++ client/src/actions/articleActions.js
@@ -6,3 +6,12 @@ export function fetchHeadlines() {
       .then(cats => dispatch({type: 'FETCH_HEADLINE_ARTICLES', payload: articles}))
   }
 }
+
+export function fetchFilteredNews(query) {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_ARTICLES'}, query: query);
+    return fetch('/api/search_all_news')
+      .then(response => response.json())
+      .then(cats => dispatch({type: 'FETCH_FILTERED_ARTICLES', payload: articles}))
+  }
+}
