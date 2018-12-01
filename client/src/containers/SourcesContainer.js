9e38f7ccdd7928a8dd8a1f37e07476902c59d547
yutingcxiang <yutingcxiang@gmail.com>
2018-12-01 18:39:55 -0500

    Remove all categories components

--- client/src/containers/SourcesContainer.js
+++ client/src/containers/SourcesContainer.js
@@ -1,38 +1,23 @@
 import React, { Component } from 'react';
 import SourcesList from '../components/articles/SourcesList';
-import CategoriesList from '../components/articles/CategoriesList';
 import { connect } from 'react-redux';
 
 class SourcesContainer extends Component {
   state = {
     showSources: false,
-    showCategories: false,
   }
 
   renderSources = event => {
     this.setState({
       showSources: true,
-      showCategories: false,
-    })
-  }
-
-  renderCategories = event => {
-    this.setState({
-      showSources: false,
-      showCategories: true
     })
   }
 
   render() {
-    const showSources = this.state.showSources
-    const showCategories = this.state.showCategories
 
     return(
       <div>
-        <button onClick={this.renderSources}>Browse By Source</button>
-        <button onClick={this.renderCategories}>Browse Category</button>
-        {showSources && <SourcesList />}
-        {showCategories && <CategoriesList />}
+        <SourcesList />}
       </div>
     )
   }
