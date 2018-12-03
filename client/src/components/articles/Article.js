ad857c9eb235bd9786133ffef6997c1ea3753dd6
yutingcxiang <yutingcxiang@gmail.com>
2018-12-03 18:46:02 -0500

    Add Semantic UI for styling

--- client/src/components/articles/Article.js
+++ client/src/components/articles/Article.js
@@ -7,6 +7,7 @@ const Article = (props) => {
       <h2><a href={props.url} target="_blank" rel="noopener noreferrer">{props.title}</a></h2>
       <p><Moment format="DD/MM/YYYY h:mm a">{props.publishedAt}</Moment></p>
       <p>{props.description}</p>
+      <br></br>
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
+import 'semantic-ui-css/semantic.min.css'
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
