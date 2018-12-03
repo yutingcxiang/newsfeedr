6ba162dce99bbade7197c5bc5448ce236f58e151
yutingcxiang <yutingcxiang@gmail.com>
2018-12-03 01:19:14 -0500

    Add error message for no results as well

--- client/src/components/articles/ResultsList.js
+++ client/src/components/articles/ResultsList.js
@@ -14,6 +14,7 @@ class ResultsList extends Component {
     return(
       <div>
         {results !== undefined && this.renderResults()}
+        {results === undefined && <p>No results found. Please try again.</p>}
       </div>
     )
   }
