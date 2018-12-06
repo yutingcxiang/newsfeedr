2e776de9455242e6d4849fd7999227f730e7a3bd
yutingcxiang <yutingcxiang@gmail.com>
2018-12-06 02:24:35 -0500

    Refactor ResultsList to be a component and SearchContainer to hold the logic

--- client/src/components/articles/ResultsList.js
+++ client/src/components/articles/ResultsList.js
@@ -1,30 +1,7 @@
 import React, { Component } from 'react';
 import Article from './Article';
-import { connect } from 'react-redux';
-import { searchArticles } from '../../actions/articleActions';
 
 class ResultsList extends Component {
-  state = {
-    page: 1
-  }
-
-  nextPage = () => {
-    this.setState(prevState => {
-      return {page: prevState.page + 1}
-    }, () => {
-        this.props.searchArticles(this.props.query, this.state.page)
-    })
-  }
-
-  previousPage = () => {
-    if (this.state.page > 1) {
-    this.setState(prevState => {
-      return {page: prevState.page - 1}
-    }, () => {
-      this.props.searchArticles(this.props.query, this.state.page)
-    })
-    }
-  }
 
   renderResults = () => {
     if (this.props.results.length > 0) {
@@ -35,35 +12,12 @@ class ResultsList extends Component {
   }
 
   render() {
-    if (this.props.results.length > 0){
-      return(<div>
-        <button onClick={this.previousPage}>Previous</button>
-        <label>{this.state.page}</label>
-        <button onClick={this.nextPage}>Next</button>
+    return(
+      <div>
         {this.renderResults()}
       </div>
     )
   }
-
-    return(
-      <div>
-      </div>
-    )
-  }
 }
 
-const mapStateToProps = state => {
-  return {
-    results: state.articles.results,
-    fetchDone: state.articles.fetchDone,
-    query: state.articles.query
-  }
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    searchArticles: (query, page) => dispatch(searchArticles(query, page))
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ResultsList);
+export default ResultsList;
--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -1,16 +1,66 @@
 import React, { Component } from 'react';
 import SearchInput from '../components/filters/SearchInput';
 import ResultsList from '../components/articles/ResultsList';
+import { connect } from 'react-redux';
+import { searchArticles } from '../actions/articleActions';
 
 class SearchContainer extends Component {
+  state = {
+    page: 1
+  }
+
+  nextPage = () => {
+    this.setState(prevState => {
+      return {page: prevState.page + 1}
+    }, () => {
+        this.props.searchArticles(this.props.query, this.state.page)
+    })
+  }
+
+  previousPage = () => {
+    if (this.state.page > 1) {
+    this.setState(prevState => {
+      return {page: prevState.page - 1}
+    }, () => {
+      this.props.searchArticles(this.props.query, this.state.page)
+    })
+    }
+  }
 
   render() {
+    let pagination;
+
+    if (this.props.results.length > 0){
+      pagination = (<div>
+        <button onClick={this.previousPage}>Previous</button>
+        <label>{this.state.page}</label>
+        <button onClick={this.nextPage}>Next</button>
+      </div>
+      )
+    }
+
     return(
       <div>
         <SearchInput searchArticles={this.props.searchArticles}/>
-        <ResultsList results={this.props.results}/>
+        {pagination}
+        <ResultsList results={this.props.results} fetchDone={this.props.fetchDone}/>
       </div>
     )
   }
 }
-export default SearchContainer;
+
+const mapStateToProps = state => {
+  return {
+    results: state.articles.results,
+    fetchDone: state.articles.fetchDone,
+    query: state.articles.query
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    searchArticles: (query, page) => dispatch(searchArticles(query, page))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchContainer);
