169a18333d90cecdcdce251f3f38bc81bac5ac84
yutingcxiang <yutingcxiang@gmail.com>
2018-12-07 13:26:49 -0500

    Install and add react-scroll-up-button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
+import ScrollUpButton from './components/layout/ScrollUpButton';
 
 class App extends Component {
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <Footer />
+        <ScrollUpButton />
       </div>
     );
   }
---
+++ client/src/components/layout/ScrollUpButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ScrollUpButton from "react-scroll-up-button";
+
+class Index extends React.Component {
+    render() {
+        return (
+            <div>
+                <ScrollUpButton />
+            </div>
+        );
+    }
+}
+
+export default ScrollUpButton;
