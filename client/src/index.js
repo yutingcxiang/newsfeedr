147821fd13c30b78c350166c79b982584ce4b055
yutingcxiang <yutingcxiang@gmail.com>
2018-12-06 22:49:02 -0500

    Setup Semantic UI

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
-import 'semantic-ui-css/semantic.min.css'
-
+// import 'semantic-ui-css/semantic.min.css'
+import './semantic/dist/semantic.min.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
