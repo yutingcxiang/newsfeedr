f7d45dc789ecb0ae8024d556bef3a613052a0882
yutingcxiang <yutingcxiang@gmail.com>
2018-12-07 21:35:29 -0500

    Adjust Procfiles

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index'
-// import 'semantic-ui-css/semantic.min.css'
 import './semantic/dist/semantic.min.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
