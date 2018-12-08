878c22d696c29dba4372276a21b611b7aa16ed61
yutingcxiang <yutingcxiang@gmail.com>
2018-12-07 19:40:06 -0500

    Add default root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "welcome#index"
+
   scope :api do
     resources :articles
       get '/top_headlines', to: 'articles#top_headlines'
