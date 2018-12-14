6fbd3f8ecbc0495a42b71ba8776c5f4ff8944c4f
yutingcxiang <yutingcxiang@gmail.com>
2018-12-14 16:14:44 -0500

    Refactor ResultsList into presentational component

--- client/src/components/articles/ResultsList.js
+++ client/src/components/articles/ResultsList.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Article from './Article';
 import SearchInput from '../filters/SearchInput';
 
-class ResultsList extends Component {
+const ResultsList = (props) => {
 
-  renderResults = () => {
-    if (this.props.numResults > 0) {
-      return(this.props.results.map((article, index) => <Article key={index} publishedAt={article.publishedAt} description={article.description} title={article.title} url={article.url} /> ))
-    } 
+  const renderResults = () => {
+    if (props.numResults > 0) {
+      return(props.results.map((article, index) => <Article key={index} publishedAt={article.publishedAt} description={article.description} title={article.title} url={article.url} /> ))
+    }
   }
 
-  render() {
-    return(
-      <div id="results-list">
-        {this.props.searchDone && <p>{this.props.numResults || 0} results found.</p>}
-        {this.props.searchDone && <div className="ui section divider"></div>}
-        {this.renderResults()}
-      </div>
-    )
+  const renderNumResults = () => {
+    if (props.searchDone) {
+      return(<p>{props.numResults || 0} results found.</p>)
+    }
   }
+
+  return(
+    <div id="results-list">
+      {renderNumResults()}
+      {renderResults()}
+    </div>
+  )
 }
 
 export default ResultsList;
